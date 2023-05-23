93ffdfc9b6f31a0014defc448f554ab307e0d191
luizclaudio <luizclaudiougulino@gmail.com>
2023-05-23 14:00:06 -0300

    0002

---
+++ src/EmprestimoFisico.java
@@ -0,0 +1,30 @@
+public class EmprestimoFisico{
+
+    PessoaFisica  pessoaFisica;
+    Luxo luxo;
+    Popular popular;
+    Suv suv;
+
+    
+
+    public EmprestimoFisico(PessoaFisica pessoaFisica, Luxo luxo) {
+        this.pessoaFisica = pessoaFisica;
+        this.luxo = luxo;
+    }
+
+    public EmprestimoFisico(PessoaFisica pessoaFisica, Popular popular) {
+        this.pessoaFisica = pessoaFisica;
+        this.popular = popular;
+    }
+
+    public EmprestimoFisico(PessoaFisica pessoaFisica, Suv suv) {
+        this.pessoaFisica = pessoaFisica;
+        this.suv = suv;
+    }
+    
+    
+
+
+
+
+}
